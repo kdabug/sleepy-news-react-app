dcebb5308c17292593302ac2126891b9aa69c2f5
Mikayda Mills <mills.ma12@gmail.com>
2019-02-20 21:30:01 -0500

    which commit is the real commit

--- news-app/src/components/DisplayArticle.jsx
+++ news-app/src/components/DisplayArticle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const DisplayArticle = props => {
   return (
     <div>
-      <img src={props.gifData.images.original.url} alt="cat" />
+      <img src={props.gifData.images.original.url} />
     </div>
   );
 };
